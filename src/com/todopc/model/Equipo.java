ecf0b9f8045b9e75e00647c70ae3b401ab4fd902
Lyoko97 <lyon_115@hotmail.com>
2025-09-25 19:11:53 -0600

    Modificacion a equipo.java

--- src/com/todopc/model/Equipo.java
+++ src/com/todopc/model/Equipo.java
@@ -1,33 +1,28 @@
 package com.todopc.model;
 
-
 public abstract class Equipo {
-private String marca;
-private String modelo;
-private double precio;
+    private String fabricante;
+    private String modelo;
+    private String microprocesador;
 
+    public Equipo(String fabricante, String modelo, String microprocesador) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.microprocesador = microprocesador;
+    }
 
-public Equipo(String marca, String modelo, double precio) {
-this.marca = marca;
-this.modelo = modelo;
-this.precio = precio;
-}
+    public String getFabricante() { return fabricante; }
+    public void setFabricante(String fabricante) { this.fabricante = fabricante; }
 
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
 
-public String getMarca() { return marca; }
-public void setMarca(String marca) { this.marca = marca; }
+    public String getMicroprocesador() { return microprocesador; }
+    public void setMicroprocesador(String microprocesador) { this.microprocesador = microprocesador; }
 
-
-public String getModelo() { return modelo; }
-public void setModelo(String modelo) { this.modelo = modelo; }
-
-
-public double getPrecio() { return precio; }
-public void setPrecio(double precio) { this.precio = precio; }
-
-
-@Override
-public String toString() {
-return String.format("Marca: %s | Modelo: %s | Precio: $%.2f", marca, modelo, precio);
-}
+    @Override
+    public String toString() {
+        return String.format("Fabricante: %s | Modelo: %s | Microprocesador: %s",
+                fabricante, modelo, microprocesador);
+    }
 }
